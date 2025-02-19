a59030a02b2cbd0d29b1c6e4fca5db72c42767fd
noobieeeeeeee <142823465+noobieeeeeeee@users.noreply.github.com>
2025-02-19 18:08:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,82 @@
+// npm install @google/generative-ai express
+
+const express = require('express');
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.API_KEY;
+
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI("AIzaSyDuBQS52fTLi9eDCfOV5Hyhk6MUILJXVls");
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "You are Lica, a friendly assistant for your Meantal Health."}],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hello! Welcome. My name is Lica."}],
+      },
+      {
+        role: "user",
+        parts: [{ text: "Hi"}],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Hi there! Thanks for reaching out to me. How can I help you"}],
+      },
+    ],
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/loader.gif', (req, res) => {
+  res.sendFile(__dirname + '/loader.gif');
+});
+app.post('/chat', async (req, res) => {
+  try {
+    const userInput = req.body?.userInput;
+    console.log('incoming /chat req', userInput)
+    if (!userInput) {
+      return res.status(400).json({ error: 'Invalid request body' });
+    }
+
+    const response = await runChat(userInput);
+    res.json({ response });
+  } catch (error) {
+    console.error('Error in chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
